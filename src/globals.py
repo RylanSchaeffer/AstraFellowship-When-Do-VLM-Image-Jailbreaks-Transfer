4924692f75eaf4c68c50963d70a2a6cef6f94dcc
Rylan <rylanschaeffer@gmail.com>
2024-02-18 23:00:59 -0800

    Launching overnight run

--- src/globals.py
+++ src/globals.py
@@ -3,7 +3,7 @@ default_config = {
         "attack_name": "ssa_common_weakness",
         "epsilon": 16.0 / 255.0,
         "step_size": 1.0 / 255.0,
-        "total_steps": 7,
+        "total_steps": 500,
     },
     "image_initialization": "NIPS17",
     "models_to_attack": "['instruct_blip','blip2']",
