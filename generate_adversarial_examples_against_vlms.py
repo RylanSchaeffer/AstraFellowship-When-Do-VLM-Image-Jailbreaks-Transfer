I have some stupid bug that won't let me backprop

generate_adversarial_examples_against_vlms.py
@@ -22,7 +22,8 @@ def generate_vlm_adversarial_examples(wandb_config: dict[str, Any]):
         # Only use one image for one attack.
         images_list: List[torch.Tensor] = [images_list[wandb_config["datum_index"]]]
     elif wandb_config["image_initialization"] == "random":
-        images_list: List[torch.Tensor] = [torch.rand((1, 3, 512, 512))]
+        image_size = wandb_config["data_kwargs"]["image_size"]
+        images_list: List[torch.Tensor] = [torch.rand((1, 3, image_size, image_size))]
     else:
         raise ValueError(
             "Invalid image_initialization: {}".format(

src/attacks/pgd.py
@@ -57,8 +57,6 @@ class PGDAttacker(AdversarialAttacker):
 
         # Ensure gradients are computed for the image.
         original_image = image.clone()
-        image = image.half()
-        image = image.cuda()
         image.requires_grad_(True)
         image.retain_grad()
 
@@ -100,13 +98,16 @@ class PGDAttacker(AdversarialAttacker):
 
             # Always calculate loss per model and use for updating the adversarial example.
             target_loss_per_model: Dict[str, torch.Tensor] = {}
-            for model_name, model_wrapper in self.models_to_attack_dict.items():
+            for model_idx, (model_name, model_wrapper) in enumerate(
+                self.models_to_attack_dict.items()
+            ):
                 target_loss_for_model = model_wrapper.compute_loss(
                     image=image,
                     prompts=batch_prompts,
                     targets=target_prompts,
                 )
                 target_loss_per_model[model_name] = target_loss_for_model
+                self.loss_history[step_idx, model_idx] = target_loss_for_model.item()
             total_target_loss = torch.mean(
                 torch.tensor(list(target_loss_per_model.values()))
             )
@@ -118,8 +119,6 @@ class PGDAttacker(AdversarialAttacker):
                 - self.attack_kwargs["step_size"] * image.grad.detach().sign()
             ).clamp(0.0, 1.0)
             image.grad.zero_()
-
-            self.loss_history.append(total_target_loss.item())
 
             wandb.log(
                 {k: v.item() for k, v in target_loss_per_model.items()},
@@ -140,7 +139,9 @@ class PGDAttacker(AdversarialAttacker):
             #         % (self.wandb_config.save_dir, step_idx),
             #     )
 
-        return original_image, image
+        attack_results = {"original_image": original_image, "adversarial_image": image}
+
+        return attack_results
 
     def plot_loss(self):
         plt.close()

src/models/llava_llama_2/visual_attacker.py
@@ -1,19 +1,18 @@
 import torch
 from tqdm import tqdm
 import random
-from llava_llama_2_utils import prompt_wrapper, generator
 from torchvision.utils import save_image
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import MultiCursor
 import seaborn as sns
 
+from src.models.llava_llama_2 import prompt_wrapper, generator
 
-def normalize(images):
-    mean = torch.tensor([0.48145466, 0.4578275, 0.40821073]).cuda()
-    std = torch.tensor([0.26862954, 0.26130258, 0.27577711]).cuda()
+
+def normalize(images: torch.Tensor) -> torch.Tensor:
+    mean = torch.tensor([0.48145466, 0.4578275, 0.40821073]).to(images.device)
+    std = torch.tensor([0.26862954, 0.26130258, 0.27577711]).to(images.device)
     images = images - mean[None, :, None, None]
     images = images / std[None, :, None, None]
     return images