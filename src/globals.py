d24e37cd3132497f209f9b0eb12f8dbe6577026c
James Chua <chuajamessh@gmail.com>
2024-04-30 03:05:45 +0800

    queue up run

--- src/globals.py
+++ src/globals.py
@@ -123,6 +123,6 @@ default_eval_config = {
         # "dataset_eval": "james_truthfulqa",
     },
     "seed": 0,
-    "wandb_run_id": "j4gbssx5",
+    "wandb_run_id": "ermz17un",
     # "wandb_sweep_id": "yvqszl4d",
 }
