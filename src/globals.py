a6d94d8ee9cb478ebb155ee843a559ffa76bc1d4
James Chua <chuajamessh@gmail.com>
2024-04-29 17:09:48 +0800

    eval survival

--- src/globals.py
+++ src/globals.py
@@ -123,6 +123,6 @@ default_eval_config = {
         # "dataset_eval": "james_truthfulqa",
     },
     "seed": 0,
-    "wandb_run_id": "lc4fybm1",
+    "wandb_run_id": "ifo702gf",
     # "wandb_sweep_id": "yvqszl4d",
 }
