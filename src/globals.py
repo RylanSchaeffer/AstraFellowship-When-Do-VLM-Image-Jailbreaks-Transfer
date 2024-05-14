5ad26a6118b99df4df44ea980a1591e55c762ef4
James Chua <chuajamessh@gmail.com>
2024-05-14 15:45:23 +0800

    try bs 16

--- src/globals.py
+++ src/globals.py
@@ -3,8 +3,8 @@ default_attack_config = {
     "compile": False,
     "data": {
         "dataset": "generated",
-        "batch_size": 8,
-        "num_workers": 4,
+        "batch_size": 16,
+        "num_workers": 1,
         "prefetch_factor": 4,
         "split": "train",
     },
