97575a1a410afde89ee4b82dacca152bb9f99d3e
James Chua <chuajamessh@gmail.com>
2024-05-14 16:15:58 +0800

    alloc

--- src/models/ensemble.py
+++ src/models/ensemble.py
@@ -109,7 +109,8 @@ class VLMEnsemble(lightning.LightningModule):
                 # TODO: Was this actually because someone else was using the GPUs at the same time?
                 vlm = vlm.to(
                     device=torch.device(
-                        f"cuda:{dmodel_device_int_str}"
+                        # f"cuda:{device_count - model_device_int_str - 1}"
+                        f"cuda:{model_device_int_str}"
                     )
                 )
 
