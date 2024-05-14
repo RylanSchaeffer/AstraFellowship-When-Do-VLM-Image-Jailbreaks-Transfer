b830eccd4955ed5a505777e28eb11be51ed7d849
James Chua <chuajamessh@gmail.com>
2024-05-14 19:58:43 +0800

    run generated

--- scripts/james/james_globals.py
+++ scripts/james/james_globals.py
@@ -2,7 +2,7 @@ default_attack_config = {
     # "compile": True,
     "compile": False,
     "data": {
-        "dataset": "wealth",
+        "dataset": "generated",
         "batch_size": 2,
         "num_workers": 1,
         "prefetch_factor": 4,
