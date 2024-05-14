0b30557ba5f655697c52f692ea31b04d5ebcde71
James Chua <chuajamessh@gmail.com>
2024-05-14 15:20:56 +0800

    fix key

--- scripts/james/model_evaluate.py
+++ scripts/james/model_evaluate.py
@@ -151,7 +151,7 @@ def evaluate_vlm_adversarial_examples():
 
         wandb_additional_data = {
             "eval_model_str": model_name_str,
-            "wandb_attack_run_id": run_jailbreak_dict["wandb_attack_run_id"],
+            "wandb_attack_run_id": run_jailbreak_dict["wandb_run_id"],
             "optimizer_step_counter": run_jailbreak_dict["optimizer_step_counter"],
             "attack_models_str": run_jailbreak_dict["attack_models_str"],
         }
