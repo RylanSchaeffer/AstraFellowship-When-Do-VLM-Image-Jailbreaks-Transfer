caa5978c60ec2bb8db7460768f61e1cb1954308d
James Chua <chuajamessh@gmail.com>
2024-05-14 20:24:20 +0800

    down bs

--- scripts/james/james_globals.py
+++ scripts/james/james_globals.py
@@ -3,7 +3,7 @@ default_attack_config = {
     "compile": False,
     "data": {
         "dataset": "generated",
-        "batch_size": 6,
+        "batch_size": 4,
         "num_workers": 1,
         "prefetch_factor": 4,
         "split": "train",
