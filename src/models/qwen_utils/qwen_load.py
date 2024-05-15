c7437029159071f59aa4b4772fe85fd194a1ba78
James Chua <chuajamessh@gmail.com>
2024-05-16 01:34:39 +0800

    don't remove last char

--- src/models/qwen_utils/qwen_load.py
+++ src/models/qwen_utils/qwen_load.py
@@ -16,6 +16,6 @@ def only_assistant_response(initial_prompt: str, response: str) -> str:
     assert starting_text in response, f"Expected {starting_text} to be in {response}"
     # remove everything before and including the assistant token
     new_response = response.split(starting_text)[1]
-    # remove the final \n
-    new_response = new_response[:-1]
+    # # remove the final \n
+    # new_response = new_response[:-1]
     return new_response
