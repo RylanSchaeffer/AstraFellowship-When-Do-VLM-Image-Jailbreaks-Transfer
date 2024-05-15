c0f4428107789794690d2bf9f47445ce93bb7a49
James Chua <chuajamessh@gmail.com>
2024-05-15 21:22:21 +0800

    try doggo

--- qwen_test_docs.py
+++ qwen_test_docs.py
@@ -30,11 +30,11 @@ model: QWenLMHeadModel = QWenLMHeadModel.from_pretrained("Qwen/Qwen-VL-Chat", de
 
 # Specify hyperparameters for generation
 model.generation_config = GenerationConfig.from_pretrained("Qwen/Qwen-VL-Chat", trust_remote_code=True)
-url = 'https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg'
+url = 'images/trina/000.jpg'
 # 1st dialogue turn
 query = tokenizer.from_list_format([
     {'image': url}, # Either a local path or an url
-    {'text': '这是什么?'},
+    {'text': 'What animal is this??'},
 ])
 
 image = load_pil_image_from_url(url)
