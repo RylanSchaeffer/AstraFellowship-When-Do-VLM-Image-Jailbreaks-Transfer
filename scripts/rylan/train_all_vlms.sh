bf5ad7c90a5b1a16ae17a93158aafe129d15a075
Rylan <rylanschaeffer@gmail.com>
2024-06-03 14:36:56 -0700

    Preparing to create eval sweeps

--- scripts/rylan/train_all_vlms.sh
+++ scripts/rylan/train_all_vlms.sh
@@ -80,42 +80,83 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 # ========================== Gemma 2B Instruct ==========================
 
+#export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+#
+## Gemma Instruct 2B + CLIP
+#torchrun --standalone --nnodes 1 --nproc-per-node 8 scripts/pretrain.py \
+#  --model.type "one-stage+7b" \
+#  --model.model_id "gemma-instruct+2b+clip" \
+#  --model.image_resize_strategy "letterbox" \
+#  --model.llm_backbone_id "gemma-2b-instruct" \
+#  --model.finetune_global_batch_size 128 \
+#  --model.finetune_per_device_batch_size 4 \
+#  --model.vision_backbone_id "clip-vit-l-336px" \
+#  --wandb_entity "rylan" \
+#  --wandb_project "prismatic-vlm"
+#
+#
+## Gemma Instruct 2B + SigLIP
+#torchrun --standalone --nnodes 1 --nproc-per-node 8 scripts/pretrain.py \
+#  --model.type "one-stage+7b" \
+#  --model.model_id "gemma-instruct+2b+siglip" \
+#  --model.image_resize_strategy "letterbox" \
+#  --model.llm_backbone_id "gemma-2b-instruct" \
+#  --model.finetune_global_batch_size 128 \
+#  --model.finetune_per_device_batch_size 4 \
+#  --model.vision_backbone_id "siglip-vit-so400m-384px" \
+#  --wandb_entity "rylan" \
+#  --wandb_project "prismatic-vlm"
+#
+#
+## Gemma Instruct 2B + DINOv2+SigLIP
+#torchrun --standalone --nnodes 1 --nproc-per-node 8 scripts/pretrain.py \
+#  --model.type "one-stage+7b" \
+#  --model.model_id "gemma-instruct+2b+dinosiglip" \
+#  --model.image_resize_strategy "letterbox" \
+#  --model.llm_backbone_id "gemma-2b-instruct" \
+#  --model.finetune_global_batch_size 128 \
+#  --model.finetune_per_device_batch_size 4 \
+#  --model.vision_backbone_id "dinosiglip-vit-so-384px" \
+#  --wandb_entity "rylan" \
+#  --wandb_project "prismatic-vlm"
+
+
+# ========================== Llama 2 7B ==========================
+
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
-# Gemma Instruct 2B + CLIP
+# Llama 2 7B + CLIP
 torchrun --standalone --nnodes 1 --nproc-per-node 8 scripts/pretrain.py \
   --model.type "one-stage+7b" \
-  --model.model_id "gemma-instruct+2b+clip" \
+  --model.model_id "llama2+7b+clip" \
   --model.image_resize_strategy "letterbox" \
-  --model.llm_backbone_id "gemma-2b-instruct" \
   --model.finetune_global_batch_size 128 \
   --model.finetune_per_device_batch_size 4 \
+  --model.llm_backbone_id "llama2-7b-pure" \
   --model.vision_backbone_id "clip-vit-l-336px" \
   --wandb_entity "rylan" \
   --wandb_project "prismatic-vlm"
 
-
-# Gemma Instruct 2B + SigLIP
+# Llama 2 7B + SigLIP
 torchrun --standalone --nnodes 1 --nproc-per-node 8 scripts/pretrain.py \
   --model.type "one-stage+7b" \
-  --model.model_id "gemma-instruct+2b+siglip" \
+  --model.model_id "llama2+7b+siglip" \
   --model.image_resize_strategy "letterbox" \
-  --model.llm_backbone_id "gemma-2b-instruct" \
   --model.finetune_global_batch_size 128 \
   --model.finetune_per_device_batch_size 4 \
+  --model.llm_backbone_id "llama2-7b-pure" \
   --model.vision_backbone_id "siglip-vit-so400m-384px" \
   --wandb_entity "rylan" \
   --wandb_project "prismatic-vlm"
 
-
-# Gemma Instruct 2B + DINOv2+SigLIP
+# Llama 2 7B + DINOv2SigLIP
 torchrun --standalone --nnodes 1 --nproc-per-node 8 scripts/pretrain.py \
   --model.type "one-stage+7b" \
-  --model.model_id "gemma-instruct+2b+dinosiglip" \
+  --model.model_id "llama2+7b+dinosiglip" \
   --model.image_resize_strategy "letterbox" \
-  --model.llm_backbone_id "gemma-2b-instruct" \
   --model.finetune_global_batch_size 128 \
   --model.finetune_per_device_batch_size 4 \
+  --model.llm_backbone_id "llama2-7b-pure" \
   --model.vision_backbone_id "dinosiglip-vit-so-384px" \
   --wandb_entity "rylan" \
   --wandb_project "prismatic-vlm"
@@ -123,7 +164,7 @@ torchrun --standalone --nnodes 1 --nproc-per-node 8 scripts/pretrain.py \
 
 # ========================== Llama 2 7B Chat ==========================
 
-## Llama 2 Chat 7B + CLIP
+### Llama 2 Chat 7B + CLIP
 #torchrun --standalone --nnodes 1 --nproc-per-node 8 scripts/pretrain.py \
 #  --model.type "one-stage+7b" \
 #  --model.model_id "llama2-chat+7b+clip" \
@@ -152,6 +193,47 @@ torchrun --standalone --nnodes 1 --nproc-per-node 8 scripts/pretrain.py \
 #  --model.vision_backbone_id "dinosiglip-vit-so-384px" \
 #  --wandb_entity "rylan" \
 #  --wandb_project "prismatic-vlm"
+
+
+# ========================== Llama 3 8B ==========================
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+# Llama 3 8B + CLIP
+torchrun --standalone --nnodes 1 --nproc-per-node 8 scripts/pretrain.py \
+  --model.type "one-stage+7b" \
+  --model.model_id "llama3+8b+clip" \
+  --model.image_resize_strategy "letterbox" \
+  --model.finetune_global_batch_size 128 \
+  --model.finetune_per_device_batch_size 4 \
+  --model.llm_backbone_id "llama3-8b-pure" \
+  --model.vision_backbone_id "clip-vit-l-336px" \
+  --wandb_entity "rylan" \
+  --wandb_project "prismatic-vlm"
+
+# Llama 3 8B + SigLIP
+torchrun --standalone --nnodes 1 --nproc-per-node 8 scripts/pretrain.py \
+  --model.type "one-stage+7b" \
+  --model.model_id "llama3+8b+siglip" \
+  --model.image_resize_strategy "letterbox" \
+  --model.finetune_global_batch_size 128 \
+  --model.finetune_per_device_batch_size 4 \
+  --model.llm_backbone_id "llama3-8b-pure" \
+  --model.vision_backbone_id "siglip-vit-so400m-384px" \
+  --wandb_entity "rylan" \
+  --wandb_project "prismatic-vlm"
+
+# Llama 3 8B + DINOv2SigLIP
+torchrun --standalone --nnodes 1 --nproc-per-node 8 scripts/pretrain.py \
+  --model.type "one-stage+7b" \
+  --model.model_id "llama3+8b+dinosiglip" \
+  --model.image_resize_strategy "letterbox" \
+  --model.finetune_global_batch_size 128 \
+  --model.finetune_per_device_batch_size 4 \
+  --model.llm_backbone_id "llama3-8b-pure" \
+  --model.vision_backbone_id "dinosiglip-vit-so-384px" \
+  --wandb_entity "rylan" \
+  --wandb_project "prismatic-vlm"
 
 
 # ========================== Llama 3 8B Instruct ==========================
--- scripts/rylan/upload_prismatic_vlms.sh
+++ scripts/rylan/upload_prismatic_vlms.sh
@@ -12,9 +12,26 @@
 #  --model_id "mistral-instruct-v0.2+7b+dinosiglip" \
 #  --run_dir "/workspace/prismatic-vlms/runs/mistral-instruct-v0.2+7b+dinosiglip+stage-finetune+x7"
 
+
+python -u upload.py \
+  --model_id "llama2+7b+clip" \
+  --run_dir "/workspace/prismatic-vlms/runs/llama2+7b+clip+stage-finetune+x7"
+
+
+python -u upload.py \
+--model_id "llama2+7b+siglip" \
+--run_dir "/workspace/prismatic-vlms/runs/llama2+7b+siglip+stage-finetune+x7"
+
+
+python -u upload.py \
+--model_id "llama2+7b+dinosiglip" \
+--run_dir "/workspace/prismatic-vlms/runs/llama2+7b+dinosiglip+stage-finetune+x7"
+
+
 #python -u upload.py \
 #  --model_id "llama2-chat+7b+clip" \
 #  --run_dir "/workspace/prismatic-vlms/runs/llama2-chat+7b+clip+stage-finetune+x7"
+
 
 #python -u upload.py \
 #--model_id "llama2-chat+7b+siglip" \
@@ -24,6 +41,7 @@
 #python -u upload.py \
 #--model_id "llama2-chat+7b+dinosiglip" \
 #--run_dir "/workspace/prismatic-vlms/runs/llama2-chat+7b+dinosiglip+stage-finetune+x7"
+
 
 #python -u upload.py \
 #--model_id "phi-instruct-3+4b+clip" \
@@ -38,9 +56,25 @@
 #--run_dir "/workspace/prismatic-vlms/runs/phi-instruct-3+4b+dinosiglip+stage-finetune+x7"
 
 
+python -u upload.py \
+--model_id "llama3+8b+clip" \
+--run_dir "/workspace/prismatic-vlms/runs/llama3+8b+clip+stage-finetune+x7"
+
+
+python -u upload.py \
+--model_id "llama3+8b+siglip" \
+--run_dir "/workspace/prismatic-vlms/runs/llama3+8b+siglip+stage-finetune+x7"
+
+
+python -u upload.py \
+--model_id "llama3+8b+clip" \
+--run_dir "/workspace/prismatic-vlms/runs/llama3+8b+dinosiglip+stage-finetune+x7"
+
+
 #python -u upload.py \
 #--model_id "llama3-instruct+8b+clip" \
 #--run_dir "runs/llama3-instruct+8b+clip+stage-finetune+x7"
+
 
 #python -u upload.py \
 #--model_id "llama3-instruct+8b+siglip" \
