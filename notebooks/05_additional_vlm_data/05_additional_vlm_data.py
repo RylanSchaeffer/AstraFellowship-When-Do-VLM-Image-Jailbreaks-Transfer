5e9b18a923627d76ea911a2985c38af11a415c0f
Rylan <rylanschaeffer@gmail.com>
2024-06-14 15:35:38 -0700

    Finished 05 additional VLM data

--- notebooks/05_additional_vlm_data/05_additional_vlm_data.py
+++ notebooks/05_additional_vlm_data/05_additional_vlm_data.py
@@ -152,11 +152,15 @@ eval_runs_histories_df.rename(
 
 eval_model_order = [
     "{'one-stage+7b'}",
-    "{'reproduction-llava-v15+7b'}",
+    "{'llava-lvis4v+7b'}",
+    "{'llava-lrv+7b'}",
+    "{'llava-lvis4v-lrv+7b'}",
 ]
-eval_model_one_or_two_stages = {
-    "{'one-stage+7b'}": "One-Stage",
-    "{'reproduction-llava-v15+7b'}": "Two-Stage",
+eval_model_training_data = {
+    "{'one-stage+7b'}": "LLaVa v1.5 Instruct",
+    "{'llava-lvis4v+7b'}": "LLaVa v1.5 Instruct\n+LVIS-Instruct-4V",
+    "{'llava-lrv+7b'}": "LLaVa v1.5 Instruct\n+LRV-Instruct",
+    "{'llava-lvis4v-lrv+7b'}": "LLaVa v1.5 Instruct\n+LVIS-Instruct-4V\n+LRV-Instruct",
 }
 
 additional_epochs_eval_runs_histories_df = eval_runs_histories_df[
@@ -164,9 +168,9 @@ additional_epochs_eval_runs_histories_df = eval_runs_histories_df[
     & (eval_runs_histories_df["Evaluated Model"].isin(eval_model_order))
 ].copy()
 additional_epochs_eval_runs_histories_df[
-    "VLM Training Stages"
+    "VLM Training Data"
 ] = additional_epochs_eval_runs_histories_df["Evaluated Model"].map(
-    eval_model_one_or_two_stages
+    eval_model_training_data
 )
 unique_metrics_order = [
     "loss/avg_epoch",
@@ -184,7 +188,7 @@ additional_epochs_eval_runs_histories_tall_df = (
         id_vars=[
             "Attack Dataset (Train Split)",
             "Eval Dataset (Val Split)",
-            "VLM Training Stages",
+            "VLM Training Data",
             "optimizer_step_counter_epoch",
         ],
         value_vars=unique_metrics_order,
@@ -213,14 +217,14 @@ g = sns.relplot(
     col="Metric",
     col_order=unique_metrics_nice_strings_order,
     col_wrap=2,
-    hue="VLM Training Stages",
-    palette="seismic",
+    hue="VLM Training Data",
+    palette=sns.color_palette("husl", len(eval_model_order)),
     linewidth=3,
     facet_kws={"margin_titles": True, "sharey": False},
 )
 g.set(xlim=(0, 50000))
 g.set_axis_labels("Gradient Step")
-g.fig.suptitle("Transfer Between VLM Trained for 1 Stage vs 2 Stages", y=1.0)
+g.fig.suptitle("Transfer Between VLM Training Data", y=1.0)
 g.set_titles(col_template="{col_name}")
 # Set the y-lim per axis
 for ax, key in zip(g.axes.flat, unique_metrics_order):
@@ -230,9 +234,9 @@ sns.move_legend(g, "upper left", bbox_to_anchor=(1.0, 1.0))
 g.legend._legend_box.align = "center"
 src.plot.save_plot_with_multiple_extensions(
     plot_dir=results_dir,
-    plot_title=f"score_vs_optimizer_step_by_num_vlm_training_stages_split_metric_lineplot",
+    plot_title=f"score_vs_optimizer_step_by_vlm_training_data_split_metric_lineplot",
 )
 # plt.show()
 
 
-print("Finished notebooks/07_one_stage_vs_two_stage!")
+print("Finished notebooks/04_additional_vlm_data!")
